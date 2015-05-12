e9eadda3977bc66b817d9175ebc44c603e4a90c5
it0a <recursive404@gmail.com>
2015-05-12 19:42:44 -0400

    Adding front end placeholder

--- src/cljs/decurse/core.cljs
+++ src/cljs/decurse/core.cljs
@@ -2,7 +2,7 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(defonce app-state (atom {:text "decur.se"}))
 
 (defn main []
   (om/root
